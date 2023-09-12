5cd4d4d765a12143ab015c8f3df1c0a23ba6b2b0
arugerdev <alvaroch250@gmail.com>
2023-09-12 23:03:54 +0200

    Fix Custom Cursor in Blog Page for Mobile 📓

--- src/routes/Blog/index.jsx
+++ src/routes/Blog/index.jsx
@@ -1,10 +1,13 @@
 import BlogPage from "../../pages/BlogPage"
 import { CustomCursor } from "../../components/CustomCursor"
+import { BrowserView } from "react-device-detect"
 
 export default function Blog() {
   return (
     <>
-      <CustomCursor />
+      <BrowserView>
+        <CustomCursor />
+      </BrowserView>
       <BlogPage />
     </>
   )
